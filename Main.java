23999332d95785bb1841bcbddc3764abef1c3a5b
unknown <sean.herbison@gmail.com>
2023-10-07 19:29:28 -0700

    Completed evenOrOdd function

--- Main.java
+++ Main.java
@@ -1,12 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(evenOrOdd(4));  // rrue
+        System.out.println(evenOrOdd(4));  // true
         System.out.println(evenOrOdd(5));  // false
     }
 
 
-    public static boolean evenOrOdd(int num) {
-        return num % 2 == 0;
+    public static boolean evenOrOdd(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean lessThan100(int[] numberList) {
+        for (int n : numberList) {
+            if (n > 100) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean repeated_names(String[] nameList) {
+        return true;
+    }
+
+    public static boolean repeated_names_set(String[] nameList) {
+        return true;
+    }
+
+    public static int[] sortList(int[] numberList) {
+        return numberList;
     }
 }
 
